08f6eac928d3605dafb824616f3eb2a10c25b008
dinoDanic <dino.danic@gmail.com>
2023-05-14 23:06:06 +0200

    client

--- src/lib/graphql-client.ts
+++ src/lib/graphql-client.ts
@@ -1,3 +1,5 @@
 import { GraphQLClient } from "graphql-request";
 
-export const graphqlClient = new GraphQLClient("http://127.0.0.1:4000/graphql");
+export const graphqlClient = new GraphQLClient(
+  "http://167.235.150.40:4000/graphql"
+);
