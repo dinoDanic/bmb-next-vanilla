49aae0a136f351e983c38b73b45b71a07ba02573
dinoDanic <dino.danic@gmail.com>
2023-05-15 22:07:17 +0200

    queies

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,21 +1,21 @@
-// import { AllProductsDocument } from "@/gql/graphql";
-// import { graphqlClient } from "@/lib";
+import { AllProductsDocument } from "@/gql/graphql";
+import { graphqlClient } from "@/lib";
 import { Stack } from "@kodiui/ui";
-// import Link from "next/link";
+import Link from "next/link";
 
 export default async function ProductsPage() {
-  // const data = await graphqlClient.request(AllProductsDocument);
+  const data = await graphqlClient.request(AllProductsDocument);
 
   return (
     <Stack>
       products:
-      {/* {data.allProducts?.map((product, i) => { */}
-      {/*   return ( */}
-      {/*     <Link href={`product/${product?.id}`} key={i}> */}
-      {/*       {product?.name} */}
-      {/*     </Link> */}
-      {/*   ); */}
-      {/* })} */}
+      {data.allProducts?.map((product, i) => {
+        return (
+          <Link href={`product/${product?.id}`} key={i}>
+            {product?.name}
+          </Link>
+        );
+      })}
     </Stack>
   );
 }
--- src/app/products2/page.tsx
+++ src/app/products2/page.tsx
@@ -1,27 +1,27 @@
 "use client";
-//
-// import { AllProductsDocument } from "@/gql/graphql";
-// import { graphqlClient } from "@/lib";
+
+import { AllProductsDocument } from "@/gql/graphql";
+import { graphqlClient } from "@/lib";
 import { Stack } from "@kodiui/ui";
-// import { useQuery } from "@tanstack/react-query";
-// import Link from "next/link";
+import { useQuery } from "@tanstack/react-query";
+import Link from "next/link";
 
 export default function ProductsPage() {
-  // const { data } = useQuery({
-  //   queryKey: ["products"],
-  //   queryFn: async () => await graphqlClient.request(AllProductsDocument),
-  // });
+  const { data } = useQuery({
+    queryKey: ["products"],
+    queryFn: async () => await graphqlClient.request(AllProductsDocument),
+  });
 
   return (
     <Stack>
       products:
-      {/* {data?.allProducts?.map((product, i) => { */}
-      {/*   return ( */}
-      {/*     <Link href={`product/${product?.id}`} key={i}> */}
-      {/*       {product?.name} */}
-      {/*     </Link> */}
-      {/*   ); */}
-      {/* })} */}
+      {data?.allProducts?.map((product, i) => {
+        return (
+          <Link href={`product/${product?.id}`} key={i}>
+            {product?.name}
+          </Link>
+        );
+      })}
     </Stack>
   );
 }
