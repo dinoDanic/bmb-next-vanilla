e51764c93162565e22b7e752a1f225a4572d219d
dinoDanic <dino.danic@gmail.com>
2023-05-21 10:31:29 +0200

    client

--- src/lib/graphql-client.ts
+++ src/lib/graphql-client.ts
@@ -1,6 +1,9 @@
 // import { graphqlApi } from "@/helpers";
 import { GraphQLClient } from "graphql-request";
 
-export const graphqlClient = new GraphQLClient(
-  "http://167.235.150.40:4000/graphql"
-);
+export const graphqlApi =
+  process.env.NODE_ENV === "production"
+    ? (process.env.NEXT_PUBLIC_GRAPHQL_API as string)
+    : "http://0.0.0.0:4000/graphql";
+
+export const graphqlClient = new GraphQLClient(graphqlApi);
